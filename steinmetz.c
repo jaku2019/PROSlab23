f9674ef6ad41614a114634a68472f032256237f3
Kuba@lab <00197407@pw.edu.pl>
2026-01-06 20:30:52 +0100

    C add check steinmetz condition funx

--- steinmetz.c
+++ steinmetz.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-double random_double(double min, double max) {
+double random_double(double min, double max){
     int random_int = rand();
     // normalizacja do [0,1]
     double normalized = (double)random_int / (double)RAND_MAX;
@@ -11,3 +11,30 @@ double random_double(double min, double max) {
     return result;
 }
 
+double random_point_in_cube(double min, double max){
+    double x = random_double(min, max);
+    double y = random_double(min, max);
+    double z = random_double(min, max);
+
+    return x, y, z;
+
+}
+
+int check_point_condition(double x, double y, double z){
+    if (x**2 + y**2 <=1) && (x**2 + z**2 <=1){
+        return 1;
+    };
+    else{
+        return 0;
+    }
+}
+
+void main(){
+    double x, y, z;
+
+    double min = -2;
+    double max = 2;
+
+    x, y, z = random_point_in_cube(min, max);
+    printf("Random point in cube: (%f, %f, %f)\n", x, y, z);
+}
